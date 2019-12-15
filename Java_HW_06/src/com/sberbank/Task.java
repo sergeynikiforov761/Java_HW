d534e63f0d29e6c8ee5256d74bbc49ba0ee0c64f
sergeynikiforov761 <nikiforov.sv@phystech.edu>
2019-12-15 21:42:48 +0300

    fixed

---
+++ Java_HW_06/src/com/sberbank/Task.java
@@ -0,0 +1,36 @@
+package com.sberbank;
+
+import java.util.concurrent.Callable;
+
+public class Task<T> {
+
+    private final Callable<? extends T> callable;
+    private volatile boolean finished = false;
+    private T result;
+    private RuntimeException exception;
+    private com.sberbank.Semaphore semaphore = new Semaphore(1);
+
+    public Task(Callable<? extends T> callable) {
+        this.callable = callable;
+    }
+
+    public T get() {
+        if (!finished) {
+            semaphore.lock();
+            if (!finished) {
+                try {
+                    result = callable.call();
+                } catch (Exception e) {
+                    exception = new RuntimeException(e);
+                    throw exception;
+                } finally {
+                    finished = true;
+                }
+            }
+            semaphore.unlock();
+        }
+
+        if (exception != null) throw exception;
+        return result;
+    }
+}
